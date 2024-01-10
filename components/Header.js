e9295ca25712991fa3cd14271f0484cd1db5b71d
juyuun <130580460+juyuun@users.noreply.github.com>
2024-01-11 01:18:30 +0900

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -9,9 +9,8 @@ const NavBar = () => {
   const BLOG = useConfig()
   const locale = useLocale()
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.MINDFULNESS, to: '/mindfulness', show: true },
-    { id: 2, name: locale.NAV.SEARCH, to: '/search', show: true }
+    { id: 0, name: locale.NAV.ARCHIEVE, to: BLOG.path || '/archieve', show: true },
+    { id: 1, name: locale.NAV.SEARCH, to: '/search', show: true }
   ]
   return (
     <div className="flex-shrink-0">
